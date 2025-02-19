5cdb69bb8bdab4b0812c3472af1496e05a651bab
Dancan Angwenyi <dancangwe@gmail.com>
2025-02-19 14:00:44 +0300

    Added comments to explain the solution

--- src/Solution.java
+++ src/Solution.java
@@ -2,30 +2,39 @@ import java.util.Stack;
 
 public class Solution {
 
-    public int solution(int[] H){
+    public int solution(int[] H) {
 
         int n = H.length;
-        if(n == 0){
-            return 0;
+        if (n == 0) {
+            return 0; // If there are no heights, no blocks are needed.
         }
 
-        //  We will use the stack to build a wall (Using the stack will optimize by reducing the number of
-        // bricks needed through stacking in an optimal way) thus ideal data structure of this problem
+        // We will use a stack to efficiently track the height of walls.
+        // The stack helps minimize the number of blocks required by ensuring
+        // we only push heights that contribute to new blocks.
 
         Stack<Integer> stack = new Stack<>();
-        int blocks = 0;
-        for(int height : H){
-            while(!stack.isEmpty() && stack.peek() > height){
-                stack.pop();
-                blocks++;
+        int blocks = 0; // Counter to track the number of blocks used.
+
+        for (int height : H) {
+
+            // If the current height is smaller than the top of the stack,
+            // it means we need to close previous blocks before continuing.
+            while (!stack.isEmpty() && stack.peek() > height) {
+                stack.pop(); // Remove the top block as it is no longer needed.
+                blocks++; // Count it as a completed block.
             }
-            if(stack.isEmpty() || stack.peek() != height){
+
+            // If the stack is empty or the top element is different from the current height,
+            // we need a new block, so we push the height onto the stack.
+            if (stack.isEmpty() || stack.peek() != height) {
                 stack.push(height);
             }
         }
 
-       return blocks + stack.size();
-
+        // The remaining elements in the stack represent blocks that were never closed,
+        // so we add their count to the total number of blocks used.
+        return blocks + stack.size();
     }
 
 }
